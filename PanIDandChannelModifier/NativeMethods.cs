ab326262d82ce6532884ad64938b4bba0039a10a
unknown <lin@pensyl.com>
2020-03-18 21:31:56 -0400

    Add somefile for test

---
+++ PanIDandChannelModifier/NativeMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.Security;
+[SuppressUnmanagedCodeSecurity()]
+public class NativeMethods
+{
+
+    [DllImport("kernal32", SetLastError = true, CharSet = CharSet.Unicode)]
+    public extern static SafeFileHandle CreateFile(string filename,
+        uint dwDesiredAccess, System.IO.FileShare dwShareMode,
+        IntPtr lp_securityAttrs,
+        System.IO.FileMode dwCreationDisposition,
+        uint dwFlagAndAttributes,
+        IntPtr hTemplateFile
+        );
+
+    //Use the file handle
+    [DllImport("kernal32", SetLastError = true)]
+    public extern static int ReadFile(
+        SafeFileHandle handle,
+        byte[] bytes,
+        int numBytesToRead,
+        out int numBytesRead,
+        IntPtr overlapped
+        );
+
+    [DllImport("kernal32", SetLastError = true)]
+    public extern static int WriteFile(SafeHandle com, 
+        byte[] bytes,
+        int numBytesToWrite,
+        out int bytesWritten,
+        IntPtr overlapped
+        );
+
+    [DllImport("kernal32", SetLastError = true)]
+    [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+    public extern static bool CloseHandle(IntPtr handle);
+
+    [DllImport("kernal32", SetLastError = true)]
+    public extern static bool SetCommState(SafeHandle com, out DCB dcb);
+
+    [DllImport("kernal32", SetLastError = true)]
+    public extern static bool SetCommTimeouts(SafeHandle com, out COMMTTIMEOUTS cOMMTTIMEOUTS);
+}
+
---
+++ PanIDandChannelModifier/YostlabDataStructure.cs
@@ -0,0 +1,120 @@
+﻿using System;
+[global::System.AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+sealed class BitfieldLengthAttribute : Attribute
+{
+    uint length;
+
+    public BitfieldLengthAttribute(uint length)
+    {
+        this.length = length;
+    }
+
+    public uint Length { get { return length; } }
+}
+
+public struct DataPacket {
+    float gyroX;
+    float gyroY;
+    float gyroZ;
+    float accX;
+    float accY;
+    float accZ;
+}
+
+/*
+ typedef struct _COMMTIMEOUTS {
+  DWORD ReadIntervalTimeout;
+  DWORD ReadTotalTimeoutMultiplier;
+  DWORD ReadTotalTimeoutConstant;
+  DWORD WriteTotalTimeoutMultiplier;
+  DWORD WriteTotalTimeoutConstant;
+} COMMTIMEOUTS, *LPCOMMTIMEOUTS;
+*/
+
+[Serializable]
+public struct COMMTTIMEOUTS {
+    uint ReadIntervalTimeout;
+    uint ReadTotalTimeoutMultiplier;
+    uint ReadTotalTimeoutConstant;
+    uint WriteTotalTimeoutMultiplier;
+    uint WriteTotalTimeoutConstant;
+}
+
+/*
+ typedef struct _DCB {
+  DWORD DCBlength;
+  DWORD BaudRate;
+  DWORD fBinary : 1;
+  DWORD fParity : 1;
+  DWORD fOutxCtsFlow : 1;
+  DWORD fOutxDsrFlow : 1;
+  DWORD fDtrControl : 2;
+  DWORD fDsrSensitivity : 1;
+  DWORD fTXContinueOnXoff : 1;
+  DWORD fOutX : 1;
+  DWORD fInX : 1;
+  DWORD fErrorChar : 1;
+  DWORD fNull : 1;
+  DWORD fRtsControl : 2;
+  DWORD fAbortOnError : 1;
+  DWORD fDummy2 : 17;
+  WORD  wReserved;
+  WORD  XonLim;
+  WORD  XoffLim;
+  BYTE  ByteSize;
+  BYTE  Parity;
+  BYTE  StopBits;
+  char  XonChar;
+  char  XoffChar;
+  char  ErrorChar;
+  char  EofChar;
+  char  EvtChar;
+  WORD  wReserved1;
+} DCB, *LPDCB;
+     */
+
+[Serializable]
+public struct DCB {
+    uint DCBlength;
+    uint BaudRate;
+    [BitfieldLength(1)]
+    uint fBinary; //1
+    [BitfieldLength(1)]
+    uint fParity; // 1
+    [BitfieldLength(1)]
+    uint fOutxCtsFlow; // 1
+    [BitfieldLength(1)]
+    uint fOutxDsrFlow; // 1
+    [BitfieldLength(2)]
+    uint fDtrControl; // 2
+    [BitfieldLength(1)]
+    uint fDsrSensitivity; // 1
+    [BitfieldLength(1)]
+    uint fTXContinueOnXoff;// 1
+    [BitfieldLength(1)]
+    uint fOutX; // 1
+    [BitfieldLength(1)]
+    uint fInX; // 1
+    [BitfieldLength(1)]
+    uint fErrorChar; // 1
+    [BitfieldLength(1)]
+    uint fNull; // 1
+    [BitfieldLength(2)]
+    uint fRtsControl; // 2
+    [BitfieldLength(1)]
+    uint fAbortOnError; // 1
+    [BitfieldLength(17)]
+    uint fDummy2; // 17
+    ushort wReserved;
+    ushort XonLim;
+    ushort XoffLim;
+    byte ByteSize;
+    byte Parity;
+    byte StopBits;
+    char XonChar;
+    char XoffChar;
+    char ErrorChar;
+    char EofChar;
+    char EvtChar;
+    ushort wReserved1;
+}
